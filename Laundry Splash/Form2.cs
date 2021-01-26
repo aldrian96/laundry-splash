d7b076f2e0c1986da4322b1d64115b9d1274f8f3
Moch Aldrian <mhmdsaldrian96@gmail.com>
2021-01-26 08:17:00 +0700

    First commit

---
+++ Laundry Splash/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laundry_Splash
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            lableWelcome.Parent = pictureBox1;
+            lableWelcome.BackColor = Color.Transparent;
+
+            lableDeskripsi.Parent = pictureBox1;
+            lableDeskripsi.BackColor = Color.Transparent;
+
+            pictureBox2.Parent = pictureBox1;
+            pictureBox2.BackColor = Color.Transparent;
+
+        }
+
+        private void password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+            Application.Exit();
+        }
+    }
+}
